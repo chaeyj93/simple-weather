7a938020ab26b46cd854db12fefc5e1ea43d2e85
LAPTOP-QLG33FQB\dul93 <chaeyj93@gmail.com>
2021-06-20 22:42:12 +0900

    Loading Screen 만들고 App.js 에서 import함

--- App.js
+++ App.js
@@ -1,40 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
+import Loading from "Loading";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.yellowView}>
-        <Text>나는노란색</Text>
-      </View>
-      <View style={styles.blueView}>
-        <Text>나는파란색</Text>
-      </View>
-      {/* <Text style={styles.text}>Hello!</Text>
-      <Text style={styles.text}>Hello!</Text> */}
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Loading></Loading>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column" /*디폴트는 column*/,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "green",
-  },
-  yellowView: {
-    flex: 1,
-    backgroundColor: "yellow",
-  },
-  blueView: {
-    flex: 2,
-    backgroundColor: "blue",
-  },
-});
