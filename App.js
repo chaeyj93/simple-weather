00b493eba481cd13a7f880e9e0d9d4ca13ab55aa
LAPTOP-QLG33FQB\dul93 <chaeyj93@gmail.com>
2021-06-20 22:50:33 +0900

    import 오타 수정

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Loading from "Loading";
+import Loading from "./Loading";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
