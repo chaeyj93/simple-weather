0772ef622c8de72d002d50e51ed0822e2058bbfc
LAPTOP-QLG33FQB\dul93 <chaeyj93@gmail.com>
2021-06-21 00:31:23 +0900

    Location 에서 1) 권한 요청하고 2) 위치정보 받아오고. + state 넣어서 페이지 관리하기 시작함

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import React from "react";
 import Loading from "./Loading";
 import * as Location from "expo-location";
+import { Alert } from "react-native";
 
 class App extends React.Component {
+  state = {
+    isLoading: true,
+  };
   myGetLocation = async () => {
-    const location = await Location.getCurrentPositionAsync({}); //https://docs.expo.io/versions/latest/sdk/location/
-    console.log(location);
+    try {
+      // throw Error();
+      //1. first, 권한 요청하고 -- 이거때문에 try - catch 사용
+      // const permissionR =
+      await Location.requestForegroundPermissionsAsync();
+      // console.log("[permission]" + permissionR);
+      //2. second, 위치 받아오기
+
+      /*
+      const { coords } = await Location.getCurrentPositionAsync(); //https://docs.expo.io/versions/latest/sdk/location/
+      console.log("[location]" + coords.latitude, coords.longitude); //여기서 내가 막 myLocation 변수선언해서 고생함 - es6 coords
+      */
+
+      const {
+        coords: { latitude, longitude }, // Api 로 보내서 날짜를 받아올 파라미터
+      } = await Location.getCurrentPositionAsync();
+
+      this.setState({ isLoading: false });
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
   };
 
   componentDidMount() {
@@ -13,7 +36,9 @@ class App extends React.Component {
   }
 
   render() {
-    return <Loading></Loading>;
+    const { isLoading } = this.state;
+    if (isLoading) return <Loading></Loading>;
+    else return null;
   }
 }
 
