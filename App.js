ff6e07a93248ffcb2140b11e75312078b57261a9
LAPTOP-QLG33FQB\dul93 <chaeyj93@gmail.com>
2021-06-20 17:49:13 +0900

    flex 까지 배움

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.yellowView}>
+        <Text>나는노란색</Text>
+      </View>
+      <View style={styles.blueView}>
+        <Text>나는파란색</Text>
+      </View>
+      {/* <Text style={styles.text}>Hello!</Text>
+      <Text style={styles.text}>Hello!</Text> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +21,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column" /*디폴트는 column*/,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow",
+  },
+  blueView: {
+    flex: 2,
+    backgroundColor: "blue",
   },
 });
